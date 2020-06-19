11116ed4ca1ea2812760015ca9aa20a11352e812
Ferdaus Kawsar <ferdaus.ahmed@gmail.com>
2020-06-19 12:30:40 -0400

    first database migration

--- videoLibrary/Controllers/CustomersController.cs
+++ videoLibrary/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,22 +11,33 @@ namespace videoLibrary.Controllers
     public class CustomersController : Controller
     {
         // GET: Customers
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
         }
-        private IEnumerable<Customer> GetCustomers()
+       /* private IEnumerable<Customer> GetCustomers()
         {
             return new List<Customer>
             {
@@ -36,6 +48,6 @@ namespace videoLibrary.Controllers
                 new Customer { Id = 5, Name = "Nafees Ahmed" }
 
             };
-        }
+        } */
     }
 }
